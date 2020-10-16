0d7fff0724d17a1230e02808b226a01f4f47986e
Michael Santo Domingo <michaelsd.dev@gmail.com>
2020-10-16 23:07:49 +1100

    require chalk and return success message to console

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const validator = require('validator')
-const getNotes = require('./notes.js')
+const chalk = require('chalk');
+const validator = require('validator');
+const getNotes = require('./notes.js');
 
-const notes = getNotes()
+const notes = getNotes();
 
-console.log(notes)
-console.log(validator.isURL('https://www.npmjs.com/package/validator'))
+console.log(notes);
+console.log(validator.isURL('https://www.npmjs.com/package/validator'));
+console.log(chalk.green('Success!'))
+console.log(chalk.green.bold.inverse('Success!'))
