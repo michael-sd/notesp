71879c8f41b149d23f13aeca3c4bd310a9eb4673
Michael Santo Domingo <michaelsd.dev@gmail.com>
2020-10-16 15:43:36 +1100

    initialise package.json and add validator package

---
+++ app.js
@@ -0,0 +1,7 @@
+const validator = require('validator')
+const getNotes = require('./notes.js')
+
+const notes = getNotes()
+
+console.log(notes)
+console.log(validator.isURL('https://www.npmjs.com/package/validator'))
