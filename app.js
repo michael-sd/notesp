8a5ef38047c6ad17e302f2250565f2ecb0d9b038
Michael Santo Domingo <michaelsd.dev@gmail.com>
2020-10-19 20:12:09 +1100

    add functions to handle add, remove, read and list notes

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const validator = require('validator');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(process.argv);
+// Add notes command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function() {
+    console.log('Adding new note');
+  }
+});
 
-if (command === 'add') {
-  console.log('adding note!')
-} else if (command === 'remove') {
-  console.log('removing note!')
-};
+// Remove notes command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log('Removing note');
+  }
+});
+// Read notes
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log('Reading note');
+  }
+});
+
+// List notes
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler: function() {
+    console.log('Listing notes');
+  }
+});
+
+console.log(yargs.argv);
